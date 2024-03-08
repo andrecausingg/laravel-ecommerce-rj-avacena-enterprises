c58707d8509c5dac662e5ee368078318194681c1
Andre <95618374+andrecausingg@users.noreply.github.com>
2024-03-08 23:36:45 +0800

    added soft delete models

--- app/Models/HistoryModel.php
+++ app/Models/HistoryModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class HistoryModel extends Model
 {
@@ -19,4 +20,5 @@ class HistoryModel extends Model
         'created_at',
         'updated_at',
     ];
+    use SoftDeletes;
 }
